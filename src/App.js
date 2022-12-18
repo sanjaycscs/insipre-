4f3661bdf9b3dd23a33c8fdc6328deb6cb4a6dd4
Sanjay <sanjaycssanjaycs221@gmail.com>
2022-12-18 13:13:42 +0530

    Sucess

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Registration form</h1>
+      <form action="#" >
+      <div>
+        <p>Username:</p>
+        <input type="text" placeholder="Username" className="username"></input>
+        <p>password:</p>
+        <input type="password" placeholder="Password" className="password"></input>
+        <p>
+          Email:
+        </p>
+        <input type="text" placeholder="Email" className="email"></input><br></br>
+        <button type="submit">Registration</button>
+      </div>
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
