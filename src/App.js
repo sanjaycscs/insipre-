871d28a4f9f9375ef8ae433c910e5e1664b48858
sanjaycscs <111496173+sanjaycscs@users.noreply.github.com>
2022-12-22 22:47:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-
+import useState from 'react';
 function App() {
+const[name,setName]=useState('');
   return (
     <div className="App">
       <h1>Welcome to Registration form</h1>
